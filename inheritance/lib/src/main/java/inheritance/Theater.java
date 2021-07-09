27f836b9a7a7d444debcbd4f69878940c4872c08
mariamodat <mariamodat0@gmail.com>
2021-07-09 18:52:28 +0300

    done lab07

--- inheritance/lib/src/main/java/inheritance/Theater.java
+++ inheritance/lib/src/main/java/inheritance/Theater.java
@@ -2,7 +2,7 @@ package inheritance;
 
 import java.util.ArrayList;
 
-public class Theater {
+public class Theater extends Review implements Reviewable {
   private final String name;
   ArrayList<Review> reviews;
   ArrayList<String> movies;
@@ -13,6 +13,44 @@ public class Theater {
     this.movies = movies;
   }
 
+  public String getName() {
+    return name;
+  }
+
+  public ArrayList<Review> getReviews() {
+    return reviews;
+  }
+
+  public ArrayList<String> getMovies() {
+    return movies;
+  }
+
+  @Override
+  public String toString() {
+    return "Theater{" +
+      "name='" + name + '\'' +
+      ", reviews=" + reviews +
+      ", movies=" + movies +
+      '}';
+  }
+
+  @Override
+  public void addReview(Review review) {
+    if (reviews.size()==0 ){
+      reviews.add(review);
+    }
+    else {
+      for (Review rev : reviews){
+        if (rev.getAuthor()== name){
+          System.out.println("  Adding reviews is allowed only once! ");
+        }
+        else {
+          reviews.add(rev);
+          setStars(rev.getStars());
+        }
+      }
+    }
+  }
 
 
 }
